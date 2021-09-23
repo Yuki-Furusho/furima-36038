6356db61b32669e9999ca3e460a034fc813f80b7
Yuki-Furusho <furusho@sikmi.com>
2021-09-23 19:39:55 +0900

    アクティブハッシュまわり修正

--- app/models/prov.rb
+++ app/models/prov.rb
@@ -1,4 +1,4 @@
-class Category < ActiveHash::Base
+class Prov < ActiveHash::Base
   self.data = [
     {id: 1, name: '---'},
     {id: 2, name: '北海道'}, 
@@ -52,5 +52,5 @@ class Category < ActiveHash::Base
 
   include ActiveHash::Associations
   has_many :items
-  
+
 end
--- app/models/ship_days.rb
+++ app/models/ship_days.rb
@@ -1,4 +1,4 @@
-class Category < ActiveHash::Base
+class ShipDays < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '1~2日で発送' },
@@ -8,5 +8,5 @@ class Category < ActiveHash::Base
 
   include ActiveHash::Associations
   has_many :items
-  
+
 end
