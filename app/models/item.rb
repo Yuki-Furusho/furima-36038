0f85c70d56908a08d7e8307b75ffe7795e2c2e21
Yuki-Furusho <furusho@sikmi.com>
2021-09-23 19:16:18 +0900

    アクティブハッシュと一部バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :prov
+  belongs_to :ship_days
+
   belongs_to :user
 
   with_options presence: true do
@@ -9,7 +16,14 @@ class Item < ApplicationRecord
     validates :delivery_fee_id
     validates :prov_id
     validates :ship_days_id
-    validates :price
+    validates :price, format: { with: /\A\d+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
     validates :user
   end
+
+  validates :category_id, numericality: { other_than: 1, message: "選択してください"}
+  validates :condition_id, numericality: { other_than: 1, message: "選択してください"}  
+  validates :delivery_fee_id, numericality: { other_than: 1, message: "選択してください"}  
+  validates :prov_id, numericality: { other_than: 1, message: "選択してください"}  
+  validates :ship_days_id, numericality: { other_than: 1, message: "選択してください"}  
+
 end
