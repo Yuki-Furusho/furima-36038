a15b1fce2f703388018f75defc912a1ca3987757
Yuki-Furusho <furusho@sikmi.com>
2021-09-24 01:11:32 +0900

    テスト作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,13 +18,13 @@ class Item < ApplicationRecord
     validates :prov_id
     validates :ship_days_id
     validates :price, format: { with: /\A\d+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-    validates :user
+    validates :image
   end
 
-  validates :category_id, numericality: { other_than: 1, message: "選択してください"}
-  validates :condition_id, numericality: { other_than: 1, message: "選択してください"}  
-  validates :delivery_fee_id, numericality: { other_than: 1, message: "選択してください"}  
-  validates :prov_id, numericality: { other_than: 1, message: "選択してください"}  
-  validates :ship_days_id, numericality: { other_than: 1, message: "選択してください"}  
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank"}  
+  validates :delivery_fee_id, numericality: { other_than: 1, message: "can't be blank"}  
+  validates :prov_id, numericality: { other_than: 1, message: "can't be blank"}  
+  validates :ship_days_id, numericality: { other_than: 1, message: "can't be blank"}  
 
 end
