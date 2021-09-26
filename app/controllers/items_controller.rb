26964d4172673d075b35ee2b2ee8c8b6b562f3fa
Yuki-Furusho <furusho@sikmi.com>
2021-09-26 15:32:30 +0900

    商品編集ページの修正
    - 同じコード部分をまとめた
    - 同じ処理をするメソッドを削除した
    - コメントアウトの説明に追記した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show]
-
+  before_action :item_find, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -21,18 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless current_user == @item.user
       redirect_to action: :index
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -46,9 +42,7 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:name, :image, :detail, :category_id, :condition_id, :delivery_fee_id, :prov_id, :ship_days_id, :price).merge(user_id: current_user.id)
   end
 
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end    
+  def item_find
+    @item = Item.find(params[:id])
   end
 end
